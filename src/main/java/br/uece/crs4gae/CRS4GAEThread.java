74568c7fa96d9d3ed2e1ff4191f69e7da51174ca
Erick Barros <erickbs7@gmail.com>
2016-12-10 21:32:36 -0300

    The Javadoc was added in the code. This project is still in progress so the documentation can still lack some information.

--- src/main/java/br/uece/crs4gae/CRS4GAEThread.java
+++ src/main/java/br/uece/crs4gae/CRS4GAEThread.java
@@ -1,26 +1,47 @@
 package br.uece.crs4gae;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.ThreadFactory;
 import com.google.appengine.api.ThreadManager;
 
+/**  
+* CRS4GAEWritingFiles.java - a class for refactor legacy code that make use of the threads and than enable this code to run in the Google App Engine.  
+* @author Marcos Borges
+* @version 1.0 
+*/ 
+
 public class CRS4GAEThread {
 	private static ThreadFactory factory = null;
-
+	
+	/**  
+	* Retrieve the ThreadFactory instance used in this class.  
+	* @return A instance of the ThreadFactory class.
+	*/ 
 	public static ThreadFactory getFactory() {
 		return factory;
 	}
-
+	
+	/**  
+	* Update the ThreadFactory instance used in this class.  
+	* @return A instance of the ThreadFactory class.
+	*/ 
 	public static void setFactory(ThreadFactory factory) {
 		CRS4GAEThread.factory = factory;
 	}
-
+	
+	/**  
+	* Retrieve a ThreadFactory instance from the runtime environment of the Google App Engine.  
+	* @return A instance of the ThreadFactory class.
+	*/ 
 	public static ThreadFactory getThreadFactoryCloud() {
 		setFactory(ThreadManager.currentRequestThreadFactory());
 		return getFactory();
 	}
-
+	
+	/**  
+	* Execute a runnable instance in the runtime environment of the Google App Engine.  
+	* @parameter A instance of aRunnable class.
+	* @return The instance of the Thread class executed in the runtime environment of the Google App Engine.
+	*/ 
 	public static Thread newCloudThread(Runnable run) {
 		return getThreadFactoryCloud().newThread(run);
 	}
--- src/main/java/br/uece/crs4gae/CRS4GAEWritingFiles.java
+++ src/main/java/br/uece/crs4gae/CRS4GAEWritingFiles.java
@@ -3,10 +3,7 @@ package br.uece.crs4gae;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
 
 import com.google.appengine.repackaged.com.google.common.io.ByteStreams;
 import com.google.appengine.tools.cloudstorage.GcsFileOptions;
@@ -16,27 +13,63 @@ import com.google.appengine.tools.cloudstorage.GcsService;
 import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
 import com.google.appengine.tools.cloudstorage.RetryParams;
 
+/**  
+* CRS4GAEWritingFiles.java - a class for refactor legacy code that make use of the file system and than enable this code to use the service Google Cloud Storage.  
+* @author Marcos Borges
+* @version 1.0 
+*/ 
 
 public class CRS4GAEWritingFiles {
 	private static String nameBucket = "MyBucket";
 	private final static GcsService gcsService = GcsServiceFactory.createGcsService(RetryParams.getDefaultInstance());
 	private static GcsOutputChannel outputChannel;
 	
+	/**  
+	* Retrieve the GcsOutputChannel instance used in this class to write files in Google Cloud Storage.  
+	* @return A instance of the GcsOutpuChannel class.
+	*/ 
 	public static GcsOutputChannel getOutputChannel() {
 		return CRS4GAEWritingFiles.outputChannel;
 	}
+	
+	/**  
+	* Update the GcsOutputChannel instance.  
+	* @parameter A instance of the GcsOutpuChannel class.
+	*/ 
 	public static void setOutputChannel(GcsOutputChannel outputChannel) {
 		CRS4GAEWritingFiles.outputChannel = outputChannel;
 	}
+	
+	/**  
+	* Retrieve the GcsService instance used in this class to communicate with the Google Cloud Storage.  
+	* @return A instance of the GcsService class.
+	*/ 
 	public static GcsService getGcsService() {
 		return gcsService;
 	}
+	
+	/**  
+	* Retrieve a GcsFilename instance that represents the complete name of a file in the Google Cloud Storage.  
+	* @parameter A string with the file name.
+	* @return A instance of the GcsFilename class.
+	*/ 
 	public static GcsFilename newCloudFile(String nameFile) {
 		return new GcsFilename(CRS4GAEWritingFiles.nameBucket, nameFile);
 	}
+	
+	/**  
+	* Create a file in the Google Cloud Storage.  
+	* @parameter A string with the file name.
+	* @return A instance of the GcsOutputChannel class.
+	*/ 
 	public static GcsOutputChannel create(GcsFilename fileName) throws IOException {
 		return getGcsService().createOrReplace(fileName, GcsFileOptions.getDefaultInstance());
 	}
+	
+	/**  
+	* Write a array of bytes in the GcsOutputChannel instance used in this class.  
+	* @parameter A array of bytes.
+	*/ 
 	public static void write(byte[] bytes) {
 		try {
 			getOutputChannel().write(ByteBuffer.wrap(bytes));
@@ -45,9 +78,18 @@ public class CRS4GAEWritingFiles {
 			e.printStackTrace();
 		}
 	}
+	
+	/**  
+	* Close the connection established through the GcsOutputChannel instance of this class.  
+	*/ 
 	public static void close() throws IOException  {
 		getOutputChannel().close();
 	}
+	
+	/**  
+	* Convert a regular file of the file system to a byte array.
+	* @parameter A InputStream instance.
+	*/ 
 	public static byte[] convertFileToByteArray(InputStream fs) throws FileNotFoundException, IOException {
 		byte[] bytes = ByteStreams.toByteArray(fs);
 		return bytes;
